b0b65df6d6f1a131364ddd2903bd3c98827695a3
hoafelizz <hoa.nguyen@felizz.com>
2017-06-18 16:50:31 +0700

    Add react-select for choose origin and destination

--- src/App.js
+++ src/App.js
@@ -3,21 +3,67 @@ import './App.css';
 import AirportStore from './stores/AirportStore';
 import AirportActions from './actions/AirportActions';
 import { Container } from 'flux/utils';
-import Autosuggest from 'react-autosuggest';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      airports: AirportStore.getState()
+      airports: AirportStore.getState(),
+      from: '',
+      to: ''
     }
+
+    this.onChangeFrom = this.onChangeFrom.bind(this);
+    this.onChangeTo = this.onChangeTo.bind(this);
   }
 
   componentDidMount() {
     AirportActions.fetchAirports();
   }
 
+  onChangeFrom(item) {
+    if(item) {
+      this.setState({
+        from: item.value
+      });
+    } else {
+      this.setState({
+        from: ''
+      });
+    }
+  }
+
+  onChangeTo(item) {
+    if(item) {
+      this.setState({
+        to: item.value
+      });
+    } else {
+      this.setState({
+        to: ''
+      });
+    }
+  }
+
   render() {
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: 'From',
+      value,
+      onChange: this.onChange
+    }
+
+    let options = this.state.airports.map(item => {
+      return Object.assign(
+        {},
+        { value: item.code },
+        { label: item.city }
+      )
+    });
+
     return (
       <div className="App">
         <header>
@@ -26,8 +72,24 @@ class App extends Component {
             <p>Check discount ticket prices and pay using your AirCheap points</p>
           </div>
           <div className="header-route">
-            {/* <Autosuggest id='origin' inputAttributes={{placeholder:'From'}} />
-            <Autosuggest id='destination' inputAttributes={{placeholder:'To'}} /> */}
+            <div className="wrap-select">
+              <Select
+                placeholder="From"
+                name="origin"
+                value={this.state.from}
+                options={options}
+                onChange={this.onChangeFrom}
+              />
+            </div>
+            <div className="wrap-select">
+              <Select
+                placeholder="To"
+                name="destination"
+                value={this.state.to}
+                options={options}
+                onChange={this.onChangeTo}
+              />
+            </div>
           </div>
         </header>
       </div>
